9835766f9a5372ade57ccb639fd6016bee506ea8
Mats de Beer <106136987+BeerB34r@users.noreply.github.com>
2024-12-02 08:47:55 +0100

    changes part1.sh to not overwrite input

--- 2/part1.sh
+++ 2/part1.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "$@" >input
+echo "$@" >part1
 sum=0
-for i in $(seq $(wc -l input | cut -f 1 -d ' '))
+for i in $(seq $(wc -l part1 | cut -f 1 -d ' '))
 	do
-		sum=$(( $sum + $(./nominal.sh $(sed -n "$i"'{p;q}' input)) ))
+		sum=$(( $sum + $(./nominal.sh $(sed -n "$i"'{p;q}' part1)) ))
 	done
 echo $sum
