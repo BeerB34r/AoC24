60e0e79b9e78eb0a05590d17b3b4d4f7c900ab80
Mats de Beer <106136987+BeerB34r@users.noreply.github.com>
2024-12-02 08:53:36 +0100

    sketch of part2 done

--- 2/part2.sh
+++ 2/part2.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
 echo "$@" >part2
+
+for i in $(seq $(wc -l part2))
+ do
+  ./separate $(sed -n "$i"'{p;q}' part2) >current
+  current=$(./part1.sh "$(cat current)")
+  if test $current -eq 0
+   then
+   else sum++
+ done
+
+echo $sum
