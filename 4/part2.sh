f46c11d3099a1a036a5a5a0e1b69b3f08211a865
Mats De Beer <mde-beer@f0r2s2.codam.nl>
2024-12-04 15:25:22 +0100

    finally finished it up, the regex is kinda ugly now tho. should probably expose a general function that lets me grab a specific subsection of a larger file... later

--- 4/part2.sh
+++ 4/part2.sh
@@ -1,6 +1,39 @@
 #!/bin/sh
 
 INPUT=$(cat input)
-SUM=0
-SUM=$(cat input | tr -d '\n' | grep -oEe 'M.S.{7}.A..{7}M.S' -e 'M.M.{7}.A..{7}S.S' -e 'S.S.{7}.A..{7}M.M' -e 'S.M.{7}.A..{7}S.M' | wc -l)
-echo $SUM
+
+crossmas(){
+if test $(sed -n $1p $3 | tail -c +$2 | head -c 3 \
+		| grep -oE "$4" | wc -l) -eq '1'
+	then	if test $(sed -n $(( $1 + 1))p $3 | tail -c +$2 | head -c 3 \
+				| grep -oE '.A.' | wc -l) -eq '1'
+				then	if test $(sed -n $(( $1 + 2))p $3 | tail -c +$2 \
+						| head -c 3 | grep -oE "$5" | wc -l) -eq '1'
+							then echo '1'
+						fi
+			fi
+fi
+}
+
+xchecks(){
+	if test $(crossmas $1 $2 $3 'M.S' 'M.S' | wc -l) -eq '1'
+		then echo '1' && return
+	fi
+	if test $(crossmas $1 $2 $3 'M.M' 'S.S' | wc -l) -eq '1'
+		then echo '1' && return
+	fi
+	if test $(crossmas $1 $2 $3 'S.M' 'S.M' | wc -l) -eq '1'
+		then echo '1' && return
+	fi
+	if test $(crossmas $1 $2 $3 'S.S' 'M.M' | wc -l) -eq '1'
+		then echo '1' && return
+	fi
+}
+
+for i in $(seq $(( $(cat input | wc -l) - 2)))
+	do
+		for j in $(seq $(( $(sed -n ${i}p input | wc -c) - 3)))
+			do
+				xchecks $i $j input
+			done
+	done | wc -l
